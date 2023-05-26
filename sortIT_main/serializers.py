9e2db8bc06a504ad9209ce444e688b0a6bc894b1
Your Namejolanta.kr <kr.jolanta@outlook.com>
2023-05-26 17:14:08 +0300

    26.05.2023 v_2

--- sortIT_main/serializers.py
+++ sortIT_main/serializers.py
@@ -5,7 +5,7 @@ from .models import Category, Type
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["id", "title"]
+        fields = ["id", "title", "image", "detail1_name", "detail1_description", "detail2_name", "detail2_description", "detail3_name", "detail3_description"]
 
 class TypeSerializer(serializers.ModelSerializer):
     class Meta:
