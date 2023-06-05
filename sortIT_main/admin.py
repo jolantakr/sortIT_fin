1df83f06cc30f94cb3ee81c61063b857d96f60bc
Your Namejolanta.kr <kr.jolanta@outlook.com>
2023-06-05 19:57:38 +0300

    05062023

--- sortIT_main/admin.py
+++ sortIT_main/admin.py
@@ -16,13 +16,16 @@ class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact', 'street', 'city', 'country')
     search_fields = ('name',)
 
+
 class EcoShopAdmin(admin.ModelAdmin):
     list_display = ('title', 'image', 'detail1_description')
     search_fields = ('title',)
 
+
 class TipsTricksAdmin(admin.ModelAdmin):
     list_display = ('title', 'image', 'detail1_description')
     search_fields = ('title',)
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Containers, ContainersAdmin)
--- sortIT_main/views.py
+++ sortIT_main/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Category, Containers, EcoShop, TipsTricks
-from .serializers import CategorySerializer, ContainersSerializer, EcoShopSerializer, TipsTricksSerializer
+from .models import Category, Containers, EcoShop, TipsTricks, Location
+from .serializers import CategorySerializer, ContainersSerializer, EcoShopSerializer, TipsTricksSerializer, LocationSerializer
 
 
 class CategoryListView(APIView):
